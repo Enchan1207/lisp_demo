c4f34902174cfb6b01b5d3a443dc9817ea010cf6
enchan <enchant0130@gmail.com>
2025-09-19 22:08:45 +0900

    feat: コマンドライン引数を受け取るようにした

--- src/fizzbuzz.lisp
+++ src/fizzbuzz.lisp
@@ -1,10 +1,20 @@
+;; fizzbuzz
 (defun fizzbuzz (x)
     (cond
         ((zerop (mod x 15)) "FizzBuzz")
         ((zerop (mod x 5)) "Buzz")
         ((zerop (mod x 3)) "Fizz")
-        (t (format nil "~d" x))
+        (t x)
     )
 )
 
-(dotimes (x 15) (write (fizzbuzz (+ x 1)))(terpri))
+;; コマンドライン引数は sb-ext:*posix-argv* という名前の特殊な変数に入る
+(defparameter n 
+    (parse-integer (cadr sb-ext:*posix-argv*))
+)
+
+(dotimes (x n) 
+    (format t "~a" (fizzbuzz (1+ x)))
+    (terpri)
+)
+
