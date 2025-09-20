e91963b6b73e2954e27404a921c900889612af9b
enchan <enchant0130@gmail.com>
2025-09-20 14:02:43 +0900

    feat: Fibonacci高速化

--- src/fibonacci.lisp
+++ src/fibonacci.lisp
@@ -1,15 +1,32 @@
 ;; n番目のフィボナッチ数
 (defun fibonacci (x)
-    (cond 
+    (cond
         ((= x 0) 0)
         ((= x 1) 1)
         ((> x 1) (+ (fibonacci (- x 2)) (fibonacci (- x 1))))
     )
 )
 
+(defun fast-fibonacci (x)
+    ;; ローカル関数
+    (labels
+        (
+            (fib (i fib-n fib-n+1)
+                (if (= i x)
+                    fib-n
+                    (fib (1+ i) fib-n+1 (+ fib-n fib-n+1) )
+                )
+            )
+        )
+        
+
+        (fib 0 0 1)
+    )
+)
+
 ;; コマンドライン引数は sb-ext:*posix-argv* という名前の特殊な変数に入る
-(defparameter n 
+(defparameter n
     (parse-integer (cadr sb-ext:*posix-argv*))
 )
 
-(print (fibonacci n))
+(print (fast-fibonacci n))
